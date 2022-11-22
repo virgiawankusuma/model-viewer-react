fe34c6ffdff82d071f2a4c00248c3c08514d95e0
virgiawankusuma <virgiawan150@gmail.com>
2022-11-22 23:16:01 +0700

    Add hotspot button

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,6 +1,11 @@
 import '@google/model-viewer';
 
 export default function Model() {
+  function linkHandler(e, url) {
+    e.preventDefault();
+    window.open(url, '_blank');
+  }
+
   return (
     <model-viewer
       id="model"
@@ -16,13 +21,18 @@ export default function Model() {
       autoplay
     >
       <button
-        class="Hotspot"
+        onClick={(e) => linkHandler(e, 'https://sekurovillage.com')}
+        className="relative z-10 w-2 h-2 p-2 bg-white rounded-full shadow-lg flex items-center justify-center"
         slot="hotspot-1"
-        data-position="-0.24238125964406976m 3.6610296115703345m -17.166091273942357m"
-        data-normal="0.24806833274829235m 0.3541736323103048m 0.9016779582886605m"
+        data-position="-0.54 0.93 0.1"
+        data-normal="-0.73 0.05 0.69"
         data-visibility-attribute="visible"
       >
-        <div class="HotspotAnnotation">Dragons!</div>
+        <div
+          className="bg-white/60 text-black font-bold rounded-full py-2 px-3 text-sm shadow-lg cursor-pointer hover:bg-white/80 transition-all duration-200 ease-in-out block absolute w-max left-[calc(100%+.4rem)]"
+        >
+          Visit Our Website
+        </div>
       </button>
     </model-viewer>
   )
