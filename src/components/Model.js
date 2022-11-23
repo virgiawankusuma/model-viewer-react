385ad41854d61866065a8eb611718e5cb1dbe653
virgiawankusuma <virgiawan150@gmail.com>
2022-11-23 15:21:20 +0700

    change coordinate

--- src/components/Model.js
+++ src/components/Model.js
@@ -6,8 +6,17 @@ export default function Model() {
     window.open(url, '_blank');
   }
 
+  const getCord = async (event) => 
+  { 
+    console.log(event.screenX, event.screenY) 
+    const modelViewer = document.querySelector('#model')
+    const vals = modelViewer.positionAndNormalFromPoint( event.screenX, event.screenY )
+    console.log(vals) 
+  }
+
   return (
     <model-viewer
+      // onClick={getCord}
       id="model"
       src="./models/RobotExpressive.glb"
       ios-src=""
@@ -15,21 +24,23 @@ export default function Model() {
       shadow-intensity="1"
       camera-controls
       ar
-      ar-modes="scene-viewer webxr quick-look"
+      // ar-modes="scene-viewer webxr quick-look"
+      ar-modes="webxr quick-look scene-viewer"
+      // ar-modes="quick-look webxr scene-viewer"
       camera-orbit="17.95deg 85.97deg 16.01m"
       auto-rotate
       autoplay
     >
       <button
         onClick={(e) => linkHandler(e, 'https://sekurovillage.com')}
-        className="relative z-10 w-2 h-2 p-2 bg-white rounded-full shadow-lg flex items-center justify-center"
+        className="relative z-10 p-[6px] bg-white rounded-full shadow-lg flex items-center justify-center animate-pulse hover:animate-none"
         slot="hotspot-1"
-        data-position="-0.54 0.93 0.1"
-        data-normal="-0.73 0.05 0.69"
+        data-position="0.6905173210524418 2.327633420972052 0.24685388734178382"
+        data-normal="0.00023907706874392375 -0.9999999709356447 0.00003115870601969318"
         data-visibility-attribute="visible"
       >
         <div
-          className="bg-white/60 text-black font-bold rounded-full py-2 px-3 text-sm shadow-lg cursor-pointer hover:bg-white/80 transition-all duration-200 ease-in-out block absolute w-max left-[calc(100%+.4rem)]"
+          className="bg-white/80 text-black font-bold rounded-full py-2 px-3 text-sm shadow-lg cursor-pointer hover:bg-white transition-all duration-200 ease-in-out block absolute w-max left-[calc(100%+.3rem)]"
         >
           Visit Our Website
         </div>
